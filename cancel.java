3ca765ea1e2a7760e8ae3120f0d8ce0c165b947a
Abdelrahman Esmat <60976246+Esmat2020@users.noreply.github.com>
2020-11-27 22:50:31 +0200

    Software-Engineering

---
+++ cancel.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sw;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ayman
+ */
+public class cancel extends userInfo implements notification{
+    String template;
+    String item;
+    
+    public void setItem(String setItem) {
+        item = setItem;
+    }
+
+    public String getItem(String getItem) {
+        System.out.println("enter the item you want to cancel");
+        Scanner userinput = new Scanner(System.in);
+        getItem = userinput.nextLine();
+        return getItem;
+
+    }
+
+    @Override
+    public void CreateTemplate() {
+        template = "your cancelled "+item+ "successfully";
+    }
+
+    @Override
+    public void UpdateTemplate(String newTemplate) {
+        this.template = newTemplate;
+
+    }
+}
